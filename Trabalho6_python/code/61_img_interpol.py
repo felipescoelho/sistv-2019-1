320e16957e2784cabc7bd7125196f6e050f9d4f4
felipescoelho <luizfscoelho.92@gmail.com>
2019-05-27 20:12:45 -0300

    6.1. is done

--- Trabalho6_python/code/61_img_interpol.py
+++ Trabalho6_python/code/61_img_interpol.py
@@ -8,6 +8,51 @@
 # 1. TAREFA:
 
 import matplotlib.pyplot as plt
+from funcs6 import subamostragem as sb
+from funcs6 import zoh_interpol as zoh
+from funcs6 import rgb2ycbcr
 
-img = plt.imread('../imgs/mandrill.tiff')
-
+img = plt.imread('../imgs/mandrill.tif')
+# Converting to YCbCr:
+img_aux = rgb2ycbcr(img)
+img_y = img_aux[0:, 0:, 0]
+# downsampling
+img_sb2 = sb(img_y, 2)
+img_sb4 = sb(img_y, 4)
+img_sb8 = sb(img_y, 8)
+# interpolation
+img_int2 = zoh(img_sb2, 2)
+img_int4 = zoh(img_sb4, 4)
+img_int8 = zoh(img_sb8, 8)
+# plotting
+dpi = 80.3
+# Downsampling factor 2:
+lx_int2, ly_int2 = img_int2.shape
+lx_int2_inch = float(lx_int2)/dpi
+ly_int2_inch = float(ly_int2)/dpi
+plt.figure(figsize=(lx_int2_inch, ly_int2_inch))
+int2 = plt.subplot(111)
+int2.imshow(img_int2, cmap='gray')
+plt.subplots_adjust(wspace=0, hspace=0, top=1, right=1, bottom=0, left=0)
+plt.savefig('../imgs/mandrill_sub2.eps', bbox_inches='tight', pad_inches=0)
+int2.axis('off')
+# Downsampling factor 4:
+lx_int4, ly_int4 = img_int4.shape
+lx_int4_inch = float(lx_int4)/dpi
+ly_int4_inch = float(ly_int4)/dpi
+plt.figure(figsize=(lx_int4_inch, ly_int4_inch))
+int4 = plt.subplot(111)
+int4.imshow(img_int4, cmap='gray')
+plt.subplots_adjust(wspace=0, hspace=0, top=1, right=1, bottom=0, left=0)
+plt.savefig('../imgs/mandrill_sub4.eps', bbox_inches='tight', pad_inches=0)
+int4.axis('off')
+# Downsampling factor 8:
+lx_int8, ly_int8 = img_int8.shape
+lx_int8_inch = float(lx_int8)/dpi
+ly_int8_inch = float(ly_int8)/dpi
+plt.figure(figsize=(lx_int8_inch, ly_int8_inch))
+int8 = plt.subplot(111)
+int8.imshow(img_int8, cmap='gray')
+plt.subplots_adjust(wspace=0, hspace=0, top=1, right=1, bottom=0, left=0)
+plt.savefig('../imgs/mandrill_sub8.eps', bbox_inches='tight', pad_inches=0)
+int8.axis('off')
--- Trabalho6_python/code/funcs6.py
+++ Trabalho6_python/code/funcs6.py
@@ -7,6 +7,47 @@ if __name__ == '__main__':
     print('Ester aquivo possui apenas funcoes.')
 
 
+def rgb2ycbcr(rgb):
+    """
+    Transforms RGB into YCbCr:
+    >>>>> ycbcr = rgb2ycbcr(rgb) <<<<<
+    where, rgb stands for the rgb array format
+           ycbcr stands for the ycbcr array format
+    """
+    # Defining array shape:
+    lx, ly, k = rgb.shape
+    # Separating RGB arrays:
+    r = rgb[0:, 0:, 0]
+    g = rgb[0:, 0:, 1]
+    b = rgb[0:, 0:, 2]
+    # Creating Y, Cb and Cr arrays:
+    y = r*.2126 + g*.7152 + b*.0722
+    cb = (b-y)/1.8556
+    cr = (r-y)/1.5748
+    # I, Q normalization:
+    min_y = y.min()
+    min_cb = cb.min()  # maior valor Cb
+    min_cr = cr.min()  # maior valor Cr
+    min_abs = min(min_y, min_cb, min_cr)  # Maior valor de Cb ou Cr
+    y_aux = y-min_abs
+    cb_aux = cb-min_abs
+    cr_aux = cr-min_abs
+    max_y = y_aux.max()
+    max_cb = cb_aux.max()
+    max_cr = cr_aux.max()
+    max_abs = max(max_y, max_cb, max_cr)
+    norm_fact = 255/max_abs
+    y_norm = y_aux*norm_fact
+    cb_norm = cb_aux*norm_fact
+    cr_norm = cr_aux*norm_fact
+    # Reuniting arrays:
+    ycbcr = np.zeros([lx, ly, k])
+    ycbcr[0:, 0:, 0] = y_norm
+    ycbcr[0:, 0:, 1] = cb_norm
+    ycbcr[0:, 0:, 2] = cr_norm
+    return ycbcr
+
+
 def subamostragem(imagem, r):
     """
     Function for downsampling an image by a given factor.
@@ -21,8 +62,22 @@ def subamostragem(imagem, r):
     for i in np.arange(r-1, lx, r):
         for j in np.arange(r-1, ly, r):
             imagem_sub[int(i/r), int(j/r)] = imagem[i, j]
-
     return imagem_sub
+
+
+def zoh_interpol(img, r):
+    """
+    Function for the Zero Order Hold Interpolation.
+    >>>>> intr_img = zoh_interpol(img, r) <<<<<
+    where, intr_img is the post_interpolation image
+           img      is the image to be interpolated
+           r        is the interpolation factor.
+    """
+    # Horizontal repetition:
+    img_aux = np.repeat(img, r, axis=0)
+    # Vertical repetition:
+    intr_img = np.repeat(img_aux, r, axis=1)
+    return intr_img
 
 
 def bi_interpol(img, r):
@@ -34,11 +89,11 @@ def bi_interpol(img, r):
            img      is the image to be interpolated
            r        is the interpolation factor.
     """
-
+    lx, ly = img.shape
     imagem_aux = np.zeros([lx, ly])
     for i in np.arange(1, lx, 2):
         for j in np.arange(1, ly, 2):
-            imagem_aux[i, j] = imagem_sub[int(i/2), int(j/2)]
+            imagem_aux[i, j] = img[int(i/2), int(j/2)]
     # Bilinear Interpolation:
     # First the horizontal iterations:
     imagem_aux[0, 0:] = imagem_aux[1, 0:]
